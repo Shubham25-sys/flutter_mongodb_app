80d512ea8c06e67f92b8cfa7767a64012490580f
shubham <shubham.wani@binyuga.com>
2023-12-25 17:23:16 +0530

    25/12/2023 Commited RestaurentScreen with Map List & UI changes.

---
+++ lib/screens/presentation/list.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter_mongodb_app/resources/assets_manager.dart';
+
+class Resources {
+  // List A = [b,c];
+  // List b = [];
+  // List c = [];
+
+  static List<Map<String,dynamic>> maplist=[
+    {
+      'foodname':'Panner Tanduri',
+      'cost':"100",
+      'imageurl':ImageAssets.starters,
+    },
+    {
+      'foodname':'Panner Tanduri',
+      'cost':"100",
+      'imageurl':ImageAssets.starters,
+    },
+    {
+      'foodname':'Panner Tanduri',
+      'cost':"100",
+      'imageurl':ImageAssets.starters,
+    },
+
+  ];
+}
